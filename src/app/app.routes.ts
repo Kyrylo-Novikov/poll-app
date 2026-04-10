3b2cf4fc19156cd863a6897bb946b05665bb48eb
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-04-10 15:44:50 +0200

    feat(header): create and integrate header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './components/landing-page/landing-page';
+import { CreateSurveyPage } from './components/create-survey-page/create-survey-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPage,
+  },
+  {
+    path: 'create',
+    component: CreateSurveyPage,
+  },
+];
