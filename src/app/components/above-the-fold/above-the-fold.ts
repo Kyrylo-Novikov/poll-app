f4bb68a0d89d516d62288513d1dc03d14d775929
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-04-13 07:47:06 +0200

    feat(above-the-fold): create hero section component

--- src/app/components/above-the-fold/above-the-fold.ts
+++ src/app/components/above-the-fold/above-the-fold.ts
@@ -3,12 +3,13 @@ import { RouterLink } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 import { Btn } from '../../shared/components/btn/btn';
 
+/**
+ * This component displays the hero section without any code logic.
+ */
 @Component({
   selector: 'app-above-the-fold',
   imports: [NgOptimizedImage, Btn, RouterLink],
   templateUrl: './above-the-fold.html',
   styleUrl: './above-the-fold.scss',
 })
-export class AboveTheFoldComponent {
-  // creatImgSrc: string = '/assets/imgs/icons/add_circle.svg';
-}
+export class AboveTheFoldComponent {}
