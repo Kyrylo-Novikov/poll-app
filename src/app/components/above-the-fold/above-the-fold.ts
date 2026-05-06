f430b886e8503ffeb527024c536e0bc0f72eab36
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-05-06 10:42:03 +0200

    refactor(above-the-fold): move routing logic to typescript with timeout

--- src/app/components/above-the-fold/above-the-fold.ts
+++ src/app/components/above-the-fold/above-the-fold.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { Btn } from '../../shared/components/btn/btn';
+import { Router } from '@angular/router';
 
 /**
  * This component displays the hero section without any code logic.
  */
 @Component({
   selector: 'app-above-the-fold',
-  imports: [NgOptimizedImage, Btn, RouterLink],
+  imports: [NgOptimizedImage, Btn],
   templateUrl: './above-the-fold.html',
   styleUrl: './above-the-fold.scss',
 })
-export class AboveTheFoldComponent {}
+export class AboveTheFoldComponent {
+  router = inject(Router);
+  routeToCreateSurvay() {
+    setTimeout(() => {
+      this.router.navigate(['/create']);
+    }, 300);
+  }
+}
