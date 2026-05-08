71175296eaf819ff0d53538e87084b0aae887bd2
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-05-08 11:44:09 +0200

    refactor(btn): remove click handler

--- src/app/shared/components/btn/btn.ts
+++ src/app/shared/components/btn/btn.ts
@@ -10,10 +10,10 @@ import { Component, input, model, signal } from '@angular/core';
     type="button"
     class="btn"
     [class]="variant()"
-    (click)="clickHandler()"
     [class.active]="isActive()"
     [disabled]="disabled()"
   >
+    <!-- (click)="clickHandler()" -->
     <span class="btn__content">
       {{ label() }}
       <div class="btn__icon" alt=""></div>
@@ -32,12 +32,4 @@ export class Btn {
 
   /** Holds the active status of buttons */
   readonly isActive = model<boolean>(false);
-
-  /**
-   * Activates the button by setting the isActive signal too true.
-   * This adds the "active" class via template binding
-   */
-  clickHandler() {
-    this.isActive.set(true);
-  }
 }
